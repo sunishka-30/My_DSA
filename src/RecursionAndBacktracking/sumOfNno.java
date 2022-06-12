0b6841a7ec0444f2c5b1c69c09b41102495092cc
Sunishka Sharma <sunishka2002@gmail.com>
2022-06-12 22:31:03 +0530

    Recursion

---
+++ src/RecursionAndBacktracking/sumOfNno.java
@@ -0,0 +1,19 @@
+package RecursionAndBacktracking;
+
+import java.util.Scanner;
+
+public class sumOfNno {
+    public static int sum(int n)
+    {
+        if(n==0)
+            return 0;
+        return n+sum(n-1);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sum(n));
+
+    }
+}

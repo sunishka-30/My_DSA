96713930a031b3d5d0830f74bd9e8e772b707782
Sunishka Sharma <sunishka2002@gmail.com>
2022-06-12 21:27:11 +0530

    Recursion

---
+++ src/RecursionAndBacktracking/printNto1.java
@@ -0,0 +1,18 @@
+package RecursionAndBacktracking;
+
+import java.util.Scanner;
+
+public class printNto1 {
+    public static void print(int i, int n) {
+        if(i<1)
+            return;
+        System.out.println(i);
+        print(i-1,n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print(n, n);
+    }
+}
